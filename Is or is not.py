589b999937a079c4c099f13399fe627077ee8d88
Brawlstars902 <aparna.rautela@gmail.com>
2025-08-05 09:26:08 +0530

    Add files via upload

---
+++ Is or is not.py
@@ -0,0 +1,6 @@
+x=input('Please enter a number.\n')
+y=input('Please type in another number.\n')
+if x is y:
+    print(x,'is equilalent to',y,'.')
+if x is not y:
+    print(x,'is not equivalent to',y,'.')
